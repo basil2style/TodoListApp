4d2b1bcd060901f4cfe5b99080c8fe3179334d69
Basil <basiltalias92@gmail.com>
2017-02-20 15:01:47 -0500

    Added more features

--- ToDoListApp/ViewController.swift
+++ ToDoListApp/ViewController.swift
@@ -12,6 +12,9 @@ import RealmSwift
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var datasource : Results<Task>!
+    var valueToPass:String!
+    let realm = try! Realm()
+
     
     @IBOutlet weak var tableView: UITableView!
     //var task = Task.allObjects()
@@ -29,14 +32,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func reloadTable()  {
-        let realm = try! Realm()
-        datasource = realm.objects(Task)
-        tableView.reloadData()
+               datasource = realm.objects(Task)
+               tableView.reloadData()
     }
 
     
     @IBAction func newTask(_ sender: Any) {
-        performSegue(withIdentifier: "detailScreen", sender: nil)
+        performSegue(withIdentifier: "detailScreen", sender: self)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -54,8 +56,57 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+      //  let currentNote = datasource[indexPath,0]
+        
+        let currentCell = tableView.cellForRow(at: indexPath)! as UITableViewCell
+        //print(currentCell)
+        valueToPass = currentCell.textLabel?.text
+        // Segue to the second view controller
+        //print(datasource.)
+        self.performSegue(withIdentifier: "details", sender: self)
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
     
-    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: UITableViewRowActionStyle.destructive, title: "Done") {
+            (deleteAction, indexPath) -> Void in
+            
+             let listToBeDeleted = self.datasource[indexPath.row]
+            print(indexPath.row)
+            try! self.realm.write {
+                () -> Void in
+                self.realm.delete(listToBeDeleted)
+                self.reloadTable()
+            }
+            
+            }
+        let editAction = UITableViewRowAction(style: UITableViewRowActionStyle.normal, title: "Edit") {
+            (editAction, indexPath) -> Void in
+            
+        }
+        return [deleteAction,editAction]
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let secondVC = segue.destination as! ViewDetail
+       // print(valueToPass)
+        if (segue.identifier == "details") {
+            secondVC.nameReceived = valueToPass
+            secondVC.forUpdate = true
+            
+        }
+        
+        //secondVC.notesReceived =
+        if (segue.identifier == "detailScreen") {
+            //secondVC.nameReceived = valueToPass
+            secondVC.forUpdate = false
+            
+        }
+    }
 }
 
--- ToDoListApp/ViewDetail.swift
+++ ToDoListApp/ViewDetail.swift
@@ -11,10 +11,45 @@ import RealmSwift
 
 class ViewDetail: UIViewController {
     
+    var datasource : Results<Task>! = nil
+    let task = Task()
+    var nameReceived : String!
+    var notesReceived : String!
+
+    
+    var forUpdate : Bool!
     @IBOutlet weak var nameLabel: UITextField!
     @IBAction func actionSave(_ sender: Any) {
+        if !forUpdate{
         saveDetails()
+        }
+        else {
+            let realm = try! Realm()
+            let task = realm.objects(Task.self)
+            try! realm.write {
+             
+                task.setValue(nameLabel.text, forKeyPath: "name")
+                // set each person's planet property to "Earth"
+                task.setValue(notes.text!, forKeyPath: "notes")
+                
+            }
+        }
         self.navigationController?.popToRootViewController(animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+     //   print(nameReceived)
+        if forUpdate == true {
+            let realm = try! Realm()
+            datasource = try realm.objects(Task.self).filter("name = %@",nameReceived)
+            print(datasource.value(forKey: "notes") ?? String.self)
+            notes.text = String(describing: datasource.value(forKey: "notes"))
+            nameLabel.text = nameReceived
+            //Results<Task> ults = realm.where(Task.class).equalsTo("name",nameReceived).findAll
+        }
+        //  print(datasource.description)
+        
     }
     @IBOutlet weak var notes: UITextField!
     
